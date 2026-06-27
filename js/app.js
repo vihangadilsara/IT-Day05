7b892188d9db532795e9b52b6617c17fb2225ebc
vihangadilsara <vdilsara30@gmail.com.com>
2026-06-27 15:09:32 +0530

    Add additional control structures and loops examples to app.js

--- js/app.js
+++ js/app.js
@@ -61,3 +61,71 @@
 // } else {
 //     console.log("false");
 // }
+
+// -------------------------------------------
+
+// if else statement
+
+// let x = 10;
+// let y = 20;
+
+// if (x > y) {
+//     console.log("x is greater than y");
+// } else if (x < y) {
+//     console.log("x is less than y");
+// } else {
+//     console.log("x is equal to y");
+// }
+
+// -------------------------------------------
+
+// switch statement
+
+// let day = 1;
+
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+// }
+
+// -------------------------------------------
+
+// for loop
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// -------------------------------------------
+
+// while loop
+
+// let i = 0;
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+
+// -------------------------------------------
+
+// do while loop
+
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 10);
+
+
+// -------------------------------------------
+
+// function
