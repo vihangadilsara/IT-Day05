c749a6ae6d75f9f8fe3ca5ac5f6d0c02f984566e
vihangadilsara <vdilsara30@gmail.com.com>
2026-06-27 14:50:40 +0530

    Refactor index.html and add initial app.js and app.ts files

---
+++ js/app.js
@@ -0,0 +1,63 @@
+// let name = "Saman";
+
+// let age = 18;
+
+// name = "ranil";
+
+// console.log(name);
+// console.log(age);
+
+// alert("Hello icet");
+
+
+// String name = "10";
+
+// ---------------------------------
+
+// let name = "saman";
+// let age = 18;
+// let isAfterAL = false;
+// let customers = [];
+// let customer = {};
+// let salary = 7500.0;
+
+// console.log(typeof name);
+
+// --------------------------------------------
+//  arithmetic operators
+
+// + | - | * | /
+
+// let x = 10;
+// let y = 20;
+
+// let z = x / y;
+
+// console.log(z);
+
+// -------------------------------------------
+
+//  comparison operators
+
+// > | < | >= | <= | == | !=
+// >== | <== | === | !==
+
+
+// let x = 11;
+
+// let y = "11";
+
+// console.log(x===y);
+
+
+// -------------------------------------------
+
+//  logical operators
+
+//  && | || | !
+
+// if (!false) {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
